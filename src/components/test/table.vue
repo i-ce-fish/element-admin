db04cd1c530ceea335a195143ca8f02cd80b86cb
binyu <binyu>
2019-11-26 19:31:09 +0800

    table

---
+++ src/components/test/table.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <el-table
+      :data="tableObj.tableData"
+      border
+      style="width: 100%"
+    >
+      <el-table-column
+        v-for="(o,i) in tableObj.tableHead"
+        :key="o.id"
+        :index="i"
+        :prop="tableObj.tableFiled[i]"
+        :label="o"
+      ></el-table-column>
+
+      <el-table-column
+        label="操作"
+      >
+        <template slot-scope="scope">
+          <el-button type="text" size="small" @click="handleClick(scope.row)">查看</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+  </div>
+</template>
+
+<script>
+
+export default {
+  components: {},
+  props: {
+    tableObj: Object
+  },
+  data() {
+    return {}
+  },
+  created() {
+  },
+  methods: {
+    handleClick(row) {
+      this.$router.push({
+        path: 'detail', query: {
+          id: row.id
+        }
+      })
+    }
+  }
+
+}
+</script>
